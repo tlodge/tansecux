7fcf18b53b3b82ce39ebfb271b01f75e91e7139f
Tom Lodge <tlodge@gmail.com>
2021-02-10 09:46:42 +0000

    adding in qrcode functionality

--- pages/mobile/scenario2/[id].js
+++ pages/mobile/scenario2/[id].js
@@ -1,10 +1,25 @@
 import { useRouter } from 'next/router'
 import {subscribe} from '../../../lib/ably'
-import { useEffect } from 'react'
+import {useCamera} from '../../../lib/useCamera'
+import { useEffect, createRef } from 'react'
+import jsQR from 'jsqr';
 
 export default function Scenario2() {
 
   const router = useRouter()
+  const videoRef = createRef();
+  const [video, isCameraInitialised, running, setPlaying, error] = useCamera(videoRef);
+  
+  const renderCamera = ()=>{
+    return <><video
+    ref={videoRef}
+    autoPlay={true}
+    muted={true}
+    controls
+    width="auto"
+    height={400}/>   
+    </>
+  }
 
   useEffect(() => {
    
@@ -25,7 +40,7 @@ export default function Scenario2() {
           <h2 className="mb-3 text-xl font-bold">Scenario Two (mobile)</h2>
         </section>
         <section>
-          Take a QR picture please    
+          {renderCamera()}
         </section>
       </div>
      

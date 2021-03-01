93cec91a5af2bd6e9f8d16368af9b2329a7a281f
Tom Lodge <tlodge@gmail.com>
2021-03-01 14:09:11 +0000

    updated the thankyou page

--- pages/thanks/index.js
+++ pages/thanks/index.js
@@ -1,10 +1,10 @@
 
 const ThanksPage = ({id, home}) => {
     return (
-      <div id="welcome">
-          <div className="text-2xl text-center p-12"> Thanks taking part.  We really appreciate it!</div>
-          <div className="text-2xl text-center "> If you have any questions, comments or concerns, please email</div>
-          <div className="text-2xl text-center font-bold p-12"> thomas.lodge@nottingham.ac.uk</div>
+      <div className="p-4">
+          <div className="text-xl text-center"> Thanks for taking part.  We really appreciate it!</div>
+          <div className="text-xl text-center mt-4"> If you have any questions, comments or concerns, please email</div>
+          <div className="text-xl text-center font-bold mt-4">thomas.lodge@nottingham.ac.uk</div>
       </div>
     );
   };
